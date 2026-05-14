43247d17dd49ccf96517adf4d105c7291d778db7
Pietro Ballarin <pieballa@gmail.com>
2026-05-14 12:39:31 +0200

    base64 encoding

--- computer_networks/exercises/base64-encoder/main.c
+++ computer_networks/exercises/base64-encoder/main.c
@@ -4,27 +4,38 @@
 #include <stdint.h>
 
 /**
- * TODO: implementare base64
+ * Encodes a string into a base64 string
+ * @param out Output string the location of the output.
+ * @param in Input string assumes \0 terminator.
  */
-char* base64(char* out, char* in, int bytes) {
-    int current = 0;
-    int32_t block = (int32_t)*in;
-    for (int i = 0; i < 3; i++) {
-        if (0 <= block & (0xF2000000 >> i*6) <= 25) {
-            out[current++] = block & (0xF2000000 >> i*6) + 65;
-        } else if (26 <= block & (0xF2000000 >> i*6) <= 51) {
-            out[current++] = block & (0xF2000000 >> i*6) + 65 + 7;
-        } else {
-            out[current++] = block & (0xF2000000 >> i*6) + 48;
-        }
+char* base64(char* out, unsigned char* in) {
+    
+    char* mapping = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    int c = 0;
+
+    for (int i = 0; (c == 0 || out[c-1] != '=') && in[i]; i+=3) {
+        out[c++] = mapping[(in[i+0] & 0xFC)>>2];
+        out[c++] = mapping[((in[i+0] & 0x03)<<4) + ((in[i+1] & 0xF0)>>4)];
+        out[c++] = in[i+1] ? mapping[((in[i+1] & 0x0F)<<2) + ((in[i+2] & 0xC0)>>6)] : '=';
+        out[c++] = in[i+1] && in[i+2] ? mapping[in[i+2] & 0x3F] : '=';
     }
+    out[c++] = 0;
+
     return out;
 }
 
-int main() {
-    char* s = "ciao";
-    char* out = malloc(strlen(s)*2);
-    base64(out, s, strlen(s)*2);
-    printf("%s\n", out);
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Too few arguments\n");
+        return -1;
+    }
+    int arg_len = strlen(argv[1])*5/3;
+    argv[arg_len] = 0;
+    // Allocating memory for output
+    char* out = malloc(arg_len);
+    memset(out, 0, arg_len);
+    // Printing base64
+    printf("%s\n", base64(out, argv[1]));
+    free(out);
     return 0;
 }
